acc0716521595d5a7c5115b4b5b6ace1564d261e
Diogo Condêço <diogobcondeco@gmail.com>
2021-11-03 23:09:46 +0000

    added chat history

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { connect, sendMsg } from './api'
 import Header from './components/Header/Header'
+import ChatHistory from './components/ChatHistory'
 
 function App() {
+	const [state, setState] = useState({
+		chatHistory: []
+	})
+
 	useEffect(() => {
-		connect()
-	}, [])
+		connect((msg) => {
+			console.log("New Message");
+			setState({
+				chatHistory: [...state.chatHistory, msg]
+			})
+			console.log(state);
+		})
+	}, [state])
 
 	const send = () => {
 		console.log("hello");
@@ -16,6 +27,7 @@ function App() {
 	return (
 		<div className="App">
 			<Header />
+			<ChatHistory chatHistory={state.chatHistory} />
 			<button onClick={send}>Hit</button>
 		</div>
 	)
