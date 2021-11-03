0622b1a8b9d10b4cb99cdd2adaf97c9e6801d0e1
Diogo Condêço <diogobcondeco@gmail.com>
2021-11-03 23:20:07 +0000

    organizing websocket code

--- backend/main.go
+++ backend/main.go
@@ -2,51 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	"github.com/diogothedev/habla-chat-app/pkg/websocket"
 )
 
-var upgrader = websocket.Upgrader {
-	ReadBufferSize: 1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-func reader(conn *websocket.Conn) {
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		fmt.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-	}
-
-	reader(ws)
+	ws, err := websocket.Upgrade(w, r)
+    if err != nil {
+        fmt.Fprintf(w, "%+V\n", err)
+    }
+    go websocket.Writer(ws)
+    websocket.Reader(ws)
 }
 
 func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Simple Server")
-	})
-
 	http.HandleFunc("/ws", serveWs)
 }
 
